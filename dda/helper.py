9d1b1a8f1ca04552eda3f6979660bada4e5cf665
jaehee-bae <hjaeb7777@gmail.com>
2024-11-04 15:05:04 +0900

    fix: keywords로 수정, 코드 리팩토링

--- dda/helper.py
+++ dda/helper.py
@@ -4,10 +4,21 @@ import re
 import deepl
 import requests
 import json
+import transformers
+import openai
+import logging
 
+
+from typing import Optional, Tuple
+from deepeval.models import DeepEvalBaseLLM
+from anthropic import Anthropic
+from transformers import AutoModelForCausalLM, AutoTokenizer
 from openpyxl import load_workbook, Workbook
 from deepeval.key_handler import KeyValues, KEY_FILE_HANDLER
 from anthropic import Anthropic
+from langchain_openai import ChatOpenAI, AzureChatOpenAI
+from langchain_community.callbacks import get_openai_callback
+from tenacity import retry, retry_if_exception_type, wait_exponential_jitter
 
 
 valid_gpt_models = [
@@ -30,10 +41,249 @@ valid_gpt_models = [
 default_gpt_model = "gpt-4o"
 
 
-def merge_files():
-    """root_path 안에 있는 모든 excel파일을 합쳐서 하나의 최종 파일로 생성하는 함수"""
+def log_retry_error(retry_state):
+    logging.error(
+        f"OpenAI rate limit exceeded. Retrying: {retry_state.attempt_number} time(s)..."
+    )
 
-    root_path = f"./dda/data"
+
+class TargetGPTModel(DeepEvalBaseLLM):
+    def __init__(
+        self,
+        model: Optional[str] = None,
+        _openai_api_key: Optional[str] = None,
+        *args,
+        **kwargs,
+    ):
+        model_name = None
+        if isinstance(model, str):
+            model_name = model
+            if model_name not in valid_gpt_models:
+                raise ValueError(
+                    f"Invalid model. Available GPT models: {', '.join(model for model in valid_gpt_models)}"
+                )
+        elif model is None:
+            model_name = default_gpt_model
+
+        self._openai_api_key = _openai_api_key
+        # args and kwargs will be passed to the underlying model, in load_model function
+        self.args = args
+        self.kwargs = kwargs
+        super().__init__(model_name)
+
+    def load_model(self):
+        if self.should_use_azure_openai():
+            openai_api_key = KEY_FILE_HANDLER.fetch_data(
+                KeyValues.AZURE_OPENAI_API_KEY
+            )
+
+            openai_api_version = KEY_FILE_HANDLER.fetch_data(
+                KeyValues.OPENAI_API_VERSION
+            )
+            azure_deployment = KEY_FILE_HANDLER.fetch_data(
+                KeyValues.AZURE_DEPLOYMENT_NAME
+            )
+            azure_endpoint = KEY_FILE_HANDLER.fetch_data(
+                KeyValues.AZURE_OPENAI_ENDPOINT
+            )
+
+            model_version = KEY_FILE_HANDLER.fetch_data(
+                KeyValues.AZURE_MODEL_VERSION
+            )
+
+            if model_version is None:
+                model_version = ""
+
+            return AzureChatOpenAI(
+                openai_api_version=openai_api_version,
+                azure_deployment=azure_deployment,
+                azure_endpoint=azure_endpoint,
+                openai_api_key=openai_api_key,
+                model_version=model_version,
+                *self.args,
+                **self.kwargs,
+            )
+
+        return ChatOpenAI(
+            model_name=self.model_name,
+            openai_api_key=self._openai_api_key,
+            *self.args,
+            **self.kwargs,
+        )
+
+    @retry(
+        wait=wait_exponential_jitter(initial=1, exp_base=2, jitter=2, max=10),
+        retry=retry_if_exception_type(openai.RateLimitError),
+        after=log_retry_error,
+    )
+    def generate(self, prompt: str) -> Tuple[str, float]:
+        chat_model = self.load_model()
+        with get_openai_callback() as cb:
+            res = chat_model.invoke(prompt)
+            return res.content
+
+    @retry(
+        wait=wait_exponential_jitter(initial=1, exp_base=2, jitter=2, max=10),
+        retry=retry_if_exception_type(openai.RateLimitError),
+        after=log_retry_error,
+    )
+    async def a_generate(self, prompt: str) -> Tuple[str, float]:
+        chat_model = self.load_model()
+        with get_openai_callback() as cb:
+            res = await chat_model.ainvoke(prompt)
+            return res.content
+
+    def should_use_azure_openai(self):
+        value = KEY_FILE_HANDLER.fetch_data(KeyValues.USE_AZURE_OPENAI)
+        return value.lower() == "yes" if value is not None else False
+
+    def get_model_name(self):
+        if self.should_use_azure_openai():
+            return "azure openai"
+        elif self.model_name:
+            return self.model_name
+
+
+class CustomClaudeOpus(DeepEvalBaseLLM):
+    def __init__(self):
+        self.model = Anthropic(
+            api_key=KEY_FILE_HANDLER.fetch_data(KeyValues.ANTHROPIC_API_KEY)
+        )
+
+    def load_model(self):
+        return self.model
+
+    def generate(self, prompt: str):
+        client = self.load_model()
+        resp = client.messages.create(
+            model="claude-3-opus-20240229",
+            max_tokens=1024,
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+        )
+        return resp.content[0].text
+
+    async def a_generate(self, prompt: str):
+        return self.generate(prompt)
+
+    def get_model_name(self):
+        return "Clause-3 Opus"
+
+
+class CustomQwen2_3B(DeepEvalBaseLLM):
+    def __init__(self):
+        model_name = "Qwen/Qwen2.5-3B-Instruct"
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name, torch_dtype="auto", device_map="cpu"
+        )
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+        self.model = model
+        self.tokenizer = tokenizer
+
+    def load_model(self):
+        return self.model
+
+    def generate(self, prompt: str) -> str:
+        model = self.load_model()
+        messages = [
+            {
+                "role": "system",
+                "content": "You are an AI Korean assistant designed to fulfill the user's spoken requests.",
+            },
+            {"role": "user", "content": prompt},
+        ]
+        text = self.tokenizer.apply_chat_template(
+            messages, tokenize=False, add_generation_prompt=True
+        )
+        pipeline = transformers.pipeline(
+            "text-generation",
+            model=model,
+            tokenizer=self.tokenizer,
+            use_cache=True,
+            device_map="cpu",
+            max_length=2048,
+            do_sample=True,
+            top_k=5,
+            num_return_sequences=1,
+        )
+        result = pipeline(text)
+        return result[0]["generated_text"].split("<|im_start|>assistant\n")[-1]
+
+    async def a_generate(self, prompt: str) -> str:
+        return self.generate(prompt)
+
+    def get_model_name(self):
+        return "QWen-2.5 3B"
+
+
+class CustomLlama3_1B(DeepEvalBaseLLM):
+    def __init__(self):
+        model_name = "meta-llama/Llama-3.2-1B-Instruct"
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            torch_dtype="auto",
+            device_map="cpu",
+        )
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+        self.model = model
+        self.tokenizer = tokenizer
+
+    def load_model(self):
+        return self.model
+
+    def generate(self, prompt: str) -> str:
+        model = self.load_model()
+        messages = [
+            {
+                "role": "system",
+                "content": "You are an AI Korean assistant designed to fulfill the user's spoken requests.",
+            },
+            {"role": "user", "content": prompt},
+        ]
+        pipe = transformers.pipeline(
+            "text-generation",
+            model=model,
+            tokenizer=self.tokenizer,
+            use_cache=True,
+            device_map="auto",
+            max_length=1000,
+            do_sample=True,
+            top_k=5,
+            num_return_sequences=1,
+        )
+        result = pipe(
+            messages,
+        )
+        content = result[0]["generated_text"][-1].get("content", "")
+        return content
+
+    async def a_generate(self, prompt: str) -> str:
+        return self.generate(prompt)
+
+    def get_model_name(self):
+        return "Llama-3.1 1B"
+
+
+def get_target_model(name):
+    if name == "gpt-3.5-turbo-0125":
+        return TargetGPTModel(name)
+    elif name == "claude-3-opus-20240229":
+        return CustomClaudeOpus()
+    elif name == "qwen2-3b":
+        return CustomQwen2_3B()
+    elif name == "llama3-1b":
+        return CustomLlama3_1B()
+    return None
+
+
+def merge_files(root_path):
+    """root_path 안에 있는 모든 excel파일을 합쳐서 하나의 최종 파일로 생성하는 함수"""
     files = glob.glob(os.path.join(root_path, "**", "*.xlsx"), recursive=True)
 
     final_wb = Workbook()
--- dda/red_team_synthesizer.py
+++ dda/red_team_synthesizer.py
@@ -1,275 +1,21 @@
 import time
-import openai
-import logging
-import transformers
-
-from typing import Optional, Tuple
-from langchain_openai import ChatOpenAI, AzureChatOpenAI
-from langchain_community.callbacks import get_openai_callback
-from tenacity import retry, retry_if_exception_type, wait_exponential_jitter
 
 from deepeval.red_teaming import RedTeamer
-from deepeval.models import DeepEvalBaseLLM
 from deepeval.models.gpt_model_schematic import SchematicGPTModel
 from openpyxl import Workbook
 from datetime import datetime
-from anthropic import Anthropic
-from transformers import AutoModelForCausalLM, AutoTokenizer
 from deepeval.test_case import LLMTestCase
 from deepeval.metrics import BaseMetric
 from dda.helper import (
     get_titles,
     translate,
-    valid_gpt_models,
-    default_gpt_model,
     get_attack_prompt,
     get_user_prompt,
     generate_attack_prompts,
     merge_files,
+    get_target_model,
 )
-from dda.red_team_config import params
-from deepeval.key_handler import KeyValues, KEY_FILE_HANDLER
-
-
-def log_retry_error(retry_state):
-    logging.error(
-        f"OpenAI rate limit exceeded. Retrying: {retry_state.attempt_number} time(s)..."
-    )
-
-
-class TargetGPTModel(DeepEvalBaseLLM):
-    def __init__(
-        self,
-        model: Optional[str] = None,
-        _openai_api_key: Optional[str] = None,
-        *args,
-        **kwargs,
-    ):
-        model_name = None
-        if isinstance(model, str):
-            model_name = model
-            if model_name not in valid_gpt_models:
-                raise ValueError(
-                    f"Invalid model. Available GPT models: {', '.join(model for model in valid_gpt_models)}"
-                )
-        elif model is None:
-            model_name = default_gpt_model
-
-        self._openai_api_key = _openai_api_key
-        # args and kwargs will be passed to the underlying model, in load_model function
-        self.args = args
-        self.kwargs = kwargs
-        super().__init__(model_name)
-
-    def load_model(self):
-        if self.should_use_azure_openai():
-            openai_api_key = KEY_FILE_HANDLER.fetch_data(
-                KeyValues.AZURE_OPENAI_API_KEY
-            )
-
-            openai_api_version = KEY_FILE_HANDLER.fetch_data(
-                KeyValues.OPENAI_API_VERSION
-            )
-            azure_deployment = KEY_FILE_HANDLER.fetch_data(
-                KeyValues.AZURE_DEPLOYMENT_NAME
-            )
-            azure_endpoint = KEY_FILE_HANDLER.fetch_data(
-                KeyValues.AZURE_OPENAI_ENDPOINT
-            )
-
-            model_version = KEY_FILE_HANDLER.fetch_data(
-                KeyValues.AZURE_MODEL_VERSION
-            )
-
-            if model_version is None:
-                model_version = ""
-
-            return AzureChatOpenAI(
-                openai_api_version=openai_api_version,
-                azure_deployment=azure_deployment,
-                azure_endpoint=azure_endpoint,
-                openai_api_key=openai_api_key,
-                model_version=model_version,
-                *self.args,
-                **self.kwargs,
-            )
-
-        return ChatOpenAI(
-            model_name=self.model_name,
-            openai_api_key=self._openai_api_key,
-            *self.args,
-            **self.kwargs,
-        )
-
-    @retry(
-        wait=wait_exponential_jitter(initial=1, exp_base=2, jitter=2, max=10),
-        retry=retry_if_exception_type(openai.RateLimitError),
-        after=log_retry_error,
-    )
-    def generate(self, prompt: str) -> Tuple[str, float]:
-        chat_model = self.load_model()
-        with get_openai_callback() as cb:
-            res = chat_model.invoke(prompt)
-            return res.content
-
-    @retry(
-        wait=wait_exponential_jitter(initial=1, exp_base=2, jitter=2, max=10),
-        retry=retry_if_exception_type(openai.RateLimitError),
-        after=log_retry_error,
-    )
-    async def a_generate(self, prompt: str) -> Tuple[str, float]:
-        chat_model = self.load_model()
-        with get_openai_callback() as cb:
-            res = await chat_model.ainvoke(prompt)
-            return res.content
-
-    def should_use_azure_openai(self):
-        value = KEY_FILE_HANDLER.fetch_data(KeyValues.USE_AZURE_OPENAI)
-        return value.lower() == "yes" if value is not None else False
-
-    def get_model_name(self):
-        if self.should_use_azure_openai():
-            return "azure openai"
-        elif self.model_name:
-            return self.model_name
-
-
-class CustomClaudeOpus(DeepEvalBaseLLM):
-    def __init__(self):
-        self.model = Anthropic(
-            api_key=KEY_FILE_HANDLER.fetch_data(KeyValues.ANTHROPIC_API_KEY)
-        )
-
-    def load_model(self):
-        return self.model
-
-    def generate(self, prompt: str):
-        client = self.load_model()
-        resp = client.messages.create(
-            model="claude-3-opus-20240229",
-            max_tokens=1024,
-            messages=[
-                {
-                    "role": "user",
-                    "content": prompt,
-                }
-            ],
-        )
-        return resp.content[0].text
-
-    async def a_generate(self, prompt: str):
-        return self.generate(prompt)
-
-    def get_model_name(self):
-        return "Clause-3 Opus"
-
-
-class CustomQwen2_3B(DeepEvalBaseLLM):
-    def __init__(self):
-        model_name = "Qwen/Qwen2.5-3B-Instruct"
-        model = AutoModelForCausalLM.from_pretrained(
-            model_name, torch_dtype="auto", device_map="cpu"
-        )
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-        self.model = model
-        self.tokenizer = tokenizer
-
-    def load_model(self):
-        return self.model
-
-    def generate(self, prompt: str) -> str:
-        model = self.load_model()
-        messages = [
-            {
-                "role": "system",
-                "content": "You are an AI Korean assistant designed to fulfill the user's spoken requests.",
-            },
-            {"role": "user", "content": prompt},
-        ]
-        text = self.tokenizer.apply_chat_template(
-            messages, tokenize=False, add_generation_prompt=True
-        )
-        pipeline = transformers.pipeline(
-            "text-generation",
-            model=model,
-            tokenizer=self.tokenizer,
-            use_cache=True,
-            device_map="cpu",
-            max_length=2048,
-            do_sample=True,
-            top_k=5,
-            num_return_sequences=1,
-        )
-        result = pipeline(text)
-        return result[0]["generated_text"].split("<|im_start|>assistant\n")[-1]
-
-    async def a_generate(self, prompt: str) -> str:
-        return self.generate(prompt)
-
-    def get_model_name(self):
-        return "QWen-2.5 3B"
-
-
-class CustomLlama3_1B(DeepEvalBaseLLM):
-    def __init__(self):
-        model_name = "meta-llama/Llama-3.2-1B-Instruct"
-        model = AutoModelForCausalLM.from_pretrained(
-            model_name,
-            torch_dtype="auto",
-            device_map="cpu",
-        )
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-        self.model = model
-        self.tokenizer = tokenizer
-
-    def load_model(self):
-        return self.model
-
-    def generate(self, prompt: str) -> str:
-        model = self.load_model()
-        messages = [
-            {
-                "role": "system",
-                "content": "You are an AI Korean assistant designed to fulfill the user's spoken requests.",
-            },
-            {"role": "user", "content": prompt},
-        ]
-        pipe = transformers.pipeline(
-            "text-generation",
-            model=model,
-            tokenizer=self.tokenizer,
-            use_cache=True,
-            device_map="auto",
-            max_length=1000,
-            do_sample=True,
-            top_k=5,
-            num_return_sequences=1,
-        )
-        result = pipe(
-            messages,
-        )
-        content = result[0]["generated_text"][-1].get("content", "")
-        return content
-
-    async def a_generate(self, prompt: str) -> str:
-        return self.generate(prompt)
-
-    def get_model_name(self):
-        return "Llama-3.1 1B"
-
-
-def _get_target_model(name):
-    if name == "gpt-3.5-turbo-0125":
-        return TargetGPTModel(name)
-    elif name == "claude-3-opus-20240229":
-        return CustomClaudeOpus()
-    elif name == "qwen2-3b":
-        return CustomQwen2_3B()
-    elif name == "llama3-1b":
-        return CustomLlama3_1B()
-    return None
+from dda.red_team_config import params, custom_params
 
 
 def save_results(df, path, params):
@@ -319,10 +65,10 @@ def main(params):
     start = time.time()
 
     now = datetime.now().strftime("%y%m%d%H%M")
-    file_name = f"breakdown_{params['target_model']}_{params['keyword']}_{now}"
+    file_name = f"breakdown_{params['target_model']}_{params['keywords']}_{now}"
     path = f"./dda/data/{file_name}"
 
-    target_model = _get_target_model(params["target_model"])
+    target_model = get_target_model(params["target_model"])
     if target_model is None:
         raise ValueError(f"Unsupported target model: {params['target_model']}")
 
@@ -431,7 +177,8 @@ if __name__ == "__main__":
     main(params)
 
     # 2) Custom
-    custom_main(params)
+    custom_main(custom_params)
 
     # If you want to merge excel data, using merge_files function.
-    # merge_files()
+    # root_path = f"./dda/data"
+    # merge_files(root_path)
